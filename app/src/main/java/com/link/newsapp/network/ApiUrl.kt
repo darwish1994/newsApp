5e4bf6ed4ec903580f0d94a4d00f55027eea09bf
darwish <ahmed.darwish@awfarmarket.com>
2022-11-29 22:14:53 +0200

    add url constant

---
+++ app/src/main/java/com/link/newsapp/network/ApiUrl.kt
@@ -0,0 +1,14 @@
+package com.link.newsapp.network
+
+object NetworkConstant {
+    const val REQUEST_TIMEOUT = 20L
+}
+
+object ArticlesUrl {
+    const val articles = "articles"
+}
+
+object ArticlesCategories {
+    const val associated_press = "associated-press"
+    const val next_web = "the-next-web"
+}
