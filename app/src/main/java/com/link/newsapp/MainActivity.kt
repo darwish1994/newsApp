ca11cc542b297cc077e895dfc062f2d7008d1851
darwish <ahmed.darwish@awfarmarket.com>
2022-11-30 00:31:02 +0200

    start working on main

--- app/src/main/java/com/link/newsapp/MainActivity.kt
+++ app/src/main/java/com/link/newsapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.link.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.link.newsapp.common.extention.viewBinding
+import com.link.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val bindings by viewBinding(ActivityMainBinding::inflate)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(bindings.root)
+
+
+
     }
 }
