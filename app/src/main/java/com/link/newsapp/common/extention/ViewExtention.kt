10db786c5278e9795a47562c3c8412fa55e4f646
darwish <ahmed.darwish@awfarmarket.com>
2022-11-30 20:52:00 +0200

    add glide ext fun

--- app/src/main/java/com/link/newsapp/common/extention/ViewExtention.kt
+++ app/src/main/java/com/link/newsapp/common/extention/ViewExtention.kt
@@ -1,7 +1,10 @@
 package com.link.newsapp.common.extention
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.ProgressBar
+import com.bumptech.glide.Glide
+import com.link.newsapp.R
 import com.link.newsapp.network.Resource
 
 fun View.toVisible() {
@@ -17,4 +20,10 @@ fun ProgressBar.loading(status: Resource<*>) {
         toVisible()
     else
         toGone()
+}
+
+fun ImageView.loadImage(url:String){
+    Glide.with(this).load(url)
+        .error(R.drawable.gallery)
+        .into(this)
 }
