a1b0bfac0ef8fe2b75c62e61819dc0b11e840958
darwish <ahmed.darwish@awfarmarket.com>
2022-11-30 20:50:49 +0200

    create home adapter

--- app/src/main/java/com/link/newsapp/presenter/article/ArticleFragment.kt
+++ app/src/main/java/com/link/newsapp/presenter/article/ArticleFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.link.newsapp.R
+import com.link.newsapp.common.base.adapter.ItemClickListener
 import com.link.newsapp.common.dialog_util.DialogUtil
 import com.link.newsapp.common.extention.loading
 import com.link.newsapp.common.extention.observe
@@ -15,16 +16,21 @@ import com.link.newsapp.data.remote.response.ResponseWrapper
 import com.link.newsapp.databinding.DialogErrorViewBinding
 import com.link.newsapp.databinding.FragmentArticleBinding
 import com.link.newsapp.network.Resource
+import com.link.newsapp.presenter.article.list.ArticlesAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class ArticleFragment : Fragment(R.layout.fragment_article) {
+class ArticleFragment : Fragment(R.layout.fragment_article), ItemClickListener {
     private val binding by viewBinding(FragmentArticleBinding::bind)
     private val viewModel by viewModels<ArticleViewModel>()
 
+    private val adapter by lazy { ArticlesAdapter() }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.recArticles.adapter = adapter
+        adapter.setListener(this)
         loadArticles()
 
     }
@@ -32,6 +38,10 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
     private fun loadArticles() {
         if (!viewModel.getArticleLiveData().hasActiveObservers())
             observe(viewModel.getArticleLiveData(), ::articleObserver)
+        // clear adapter data
+        adapter.swipeData(arrayListOf())
+        viewModel.getArticles()
+
     }
 
     private fun articleObserver(it: Resource<ResponseWrapper<List<Article>>>?) = it?.let {
@@ -39,8 +49,8 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
         when (it) {
             is Resource.Success -> {
                 if (it.data?.status == Status.OK) {
-                    // todo append data in adapter
 
+                    adapter.appendData(it.data.data ?: arrayListOf())
                 } else {
                     it.data?.message?.let { it1 -> showError(it1) }
                 }
@@ -67,5 +77,15 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
             },
             cancel = {},
         )
+    }
+
+    override fun <T> onItemClicked(item: T) {
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        adapter.setListener(null)
     }
 }
