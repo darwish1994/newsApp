30569d91b14570f4dd651abde69d1167609934be
darwish <ahmed.darwish@awfarmarket.com>
2022-11-30 20:51:35 +0200

    change date time format add extension fun for date

---
+++ app/src/main/java/com/link/newsapp/common/extention/DateExtention.kt
@@ -0,0 +1,13 @@
+package com.link.newsapp.common.extention
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+const val timeFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+const val dayFormat = "MMM dd,yyyy"
+
+fun Date.convertToStringDate(): String {
+    val sdf = SimpleDateFormat(dayFormat, Locale.getDefault())
+    return sdf.format(this)
+}
