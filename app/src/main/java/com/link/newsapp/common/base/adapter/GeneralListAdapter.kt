9a67c64c2ccfdd88992774302f462c7c32d28420
darwish <ahmed.darwish@awfarmarket.com>
2022-11-30 20:52:45 +0200

    add fun for append data

--- app/src/main/java/com/link/newsapp/common/base/adapter/GeneralListAdapter.kt
+++ app/src/main/java/com/link/newsapp/common/base/adapter/GeneralListAdapter.kt
@@ -4,13 +4,18 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 
-abstract class GeneralListAdapter<T,VB:ViewBinding> : RecyclerView.Adapter<BaseHolder<T, VB>>(){
+abstract class GeneralListAdapter<T, VB : ViewBinding> : RecyclerView.Adapter<BaseHolder<T, VB>>() {
 
     private val mData = arrayListOf<T>()
+    private var listener: ItemClickListener? = null
 
-    fun swipeData(data:List<T>){
-        val diffUtil = GeneralDiffUtil(mData,data)
-        val diffResults =  DiffUtil.calculateDiff(diffUtil)
+    fun setListener(listener: ItemClickListener?) {
+        this.listener = listener
+    }
+
+    fun swipeData(data: List<T>) {
+        val diffUtil = GeneralDiffUtil(mData, data)
+        val diffResults = DiffUtil.calculateDiff(diffUtil)
         diffResults.dispatchUpdatesTo(this)
         mData.apply {
             clear()
@@ -18,13 +23,24 @@ abstract class GeneralListAdapter<T,VB:ViewBinding> : RecyclerView.Adapter<BaseH
         }
     }
 
+    fun appendData(data: List<T>) {
+        val diffUtil = GeneralDiffUtil(mData, data)
+        val diffResults = DiffUtil.calculateDiff(diffUtil)
+        diffResults.dispatchUpdatesTo(this)
+        mData.apply {
+            addAll(data)
+        }
+    }
+
+
     override fun getItemCount(): Int = mData.size
 
     override fun onBindViewHolder(holder: BaseHolder<T, VB>, position: Int) {
         holder.bind(mData[position])
+        holder.itemView.setOnClickListener {
+            listener?.onItemClicked(mData[position])
+        }
     }
-
-
 
 
 }
